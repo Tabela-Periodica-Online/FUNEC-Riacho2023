6721f75df60461e32b9653f7da8a1f7e974f529b
Cereal-Molhado <samueldiasdesouzabarros@gmail.com>
2023-11-10 23:01:34 -0300

    NavBar + responsivel 1
    navbar renovada + responsividade

--- js/tabela.js
+++ js/tabela.js
@@ -1,11 +1,29 @@
-var modalBtns = document.querySelectorAll(".modal-open")
+var modalBtns = document.querySelectorAll(".modal-open");
+
+const menu1 = document.querySelector('.menu');
+const navLink = document.querySelector('.nav_link');
 
 const _elements ={
     tema: document.querySelector(".switch_track"),
 }
 
-modalBtns.forEach(function(btn){
-    btn.onclick = function(){
+menu1.addEventListener('click', () => {
+    navLink.classList.toggle('hide');
+});
+
+
+_elements.tema.addEventListener("click", () => {
+    const modoescuro = _elements.tema.classList.toggle("switch_track--dark");
+
+    if (modoescuro)
+        document.documentElement.setAttribute("data-theme", "dark");
+    else
+        document.documentElement.setAttribute("data-theme", "light");
+});
+
+
+modalBtns.forEach(function (btn) {
+    btn.onclick = function () {
         var modal = btn.getAttribute("data-modal")
 
         document.getElementById(modal).style.display = "block"
@@ -14,27 +32,18 @@ modalBtns.forEach(function(btn){
 
 var closeBtns = document.querySelectorAll(".modal-close")
 
-closeBtns.forEach(function(btn){
-    btn.onclick = function(){
+closeBtns.forEach(function (btn) {
+    btn.onclick = function () {
         var modal = btn.closest(".modal").style.display = "none"
     }
 })
 
-window.onclick = function(e){
+window.onclick = function (e) {
 
-    if(e.target.classList.contains('modal')){
+    if (e.target.classList.contains('modal')) {
 
         e.target.style.display = "none"
 
     }
 
 }
-
-_elements.tema.addEventListener("click", () => {
-    const modoescuro = _elements.tema.classList.toggle("switch_track--dark");
-
-    if(modoescuro)
-        document.documentElement.setAttribute("data-theme", "dark");
-    else
-        document.documentElement.setAttribute("data-theme", "light");
-});
